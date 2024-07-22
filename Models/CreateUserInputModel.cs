d405d0c639352a49e8aeb45cd9488f209c0e912b
Thiago Alexandre <thiagoasilvapro@gmail.com>
2024-07-22 19:56:17 -0300

    Novo commit

--- Models/CreateUserInputModel.cs
+++ Models/CreateUserInputModel.cs
@@ -2,5 +2,8 @@
 {
     public class CreateUserInputModel
     {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
